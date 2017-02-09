b605d9a7f84297d87de3892282bdd73ff0b86a79
Acer Marcos <mpalacios@sollutia.com>
2017-02-09 18:14:05 +0100

    Calculadora de Kata

---
+++ src/calcular.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 09/02/2017
+ * Time: 17:35
+ */
---
+++ test/Test2.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 09/02/2017
+ * Time: 17:04
+ */
+class Test2
+{
+
+}
